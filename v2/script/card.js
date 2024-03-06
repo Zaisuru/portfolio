0e7d16d3f11b1e5d2fe9958c119a8816d2c0a842
Thomas HOLDERBAUM <thomas.holderbaum@gmail.com>
2024-03-06 17:43:57 +0100

    add v2

---
+++ v2/script/card.js
@@ -0,0 +1,63 @@
+let titleProject = [
+    "Pépinières Epaignes",
+    "KiloShop",
+    "Booki",
+    "Oh My Food"
+];
+let describeProject= [
+    "Projet pour la pépnière d'Epaignes en Normandie",
+    "Application web dans le but de géer une base de données des clients pour ensuite utiliser un outils de newsletter",
+    "Projet 2 du parcours Développeur Web chez OpenClassrooms edition 2021",
+    "Projet 5 du parcours Développeur Web chez OpenClassrooms edition 2021"
+
+];
+let imgProject = ["pep.PNG",
+"KiloShop.png",
+"booki.png",
+"booki.png"];
+
+function addElement() {
+    // declare tag element
+    for (i = 0; i < titleProject.length; i++){
+        const colDiv = document.createElement("div");
+        const img = document.createElement("img");
+        const cardDiv = document.createElement("div");
+        const cardBodyDiv = document.createElement("div");
+        const cardTitleDiv = document.createElement("div");
+        const titleCard = document.createElement("h3");
+        const paragraph = document.createElement("p");
+        const btn = document.createElement("button");
+
+        colDiv.classList.add("col-lg-4");
+        colDiv.setAttribute("id","responsive"+titleProject[i]);
+        document.getElementById("projectContainer").appendChild(colDiv);
+
+        cardDiv.classList.add("card", "projectCard");
+        cardDiv.setAttribute("id", titleProject[i]);
+        document.getElementById(colDiv.id).appendChild(cardDiv);
+
+        img.classList.add('card-img-top');
+        img.setAttribute("id", titleProject[i]);
+        img.setAttribute("src","pictures/"+imgProject[i]);
+        document.getElementById(cardDiv.id).appendChild(img);
+
+        cardBodyDiv.classList.add("card-body");
+        cardBodyDiv.setAttribute("id", "body"+titleProject[i]);
+        document.getElementById(cardDiv.id).appendChild(cardBodyDiv);
+
+        titleCard.classList.add("card-title");
+        titleCard.setAttribute("id", "title"+titleProject[i]);
+        titleCard.textContent = titleProject[i];
+        document.getElementById(cardBodyDiv.id).appendChild(titleCard);
+
+        paragraph.classList.add("card-text");
+        paragraph.setAttribute("id", "text"+titleProject[i])
+        paragraph.textContent = describeProject[i];
+        document.getElementById(cardBodyDiv.id).appendChild(paragraph);
+
+        btn.classList.add('btn','btn-primary');
+        btn.textContent = ("Voir le site");
+        document.getElementById(cardDiv.id).appendChild(btn);
+
+    }
+}
---
+++ v2/script/progressBar.js
@@ -0,0 +1,153 @@
+let frontSkill = ["HTML",
+"CSS",
+"JavaScript",
+"ReactJs"];
+let frontSkillPercent=[70,
+50,
+35,
+20];
+
+let backSkill = [
+    "Php",
+    "Angular",
+    "MySQL",
+    "MariaDB"
+];
+let backSkillPercent=[
+    25,
+    20,
+    60,
+    40
+];
+
+let otherSkill = [
+    "Administration Systèmes",
+    "Administration Réseau",
+    "PowerShell",
+    "Batch",
+    "Bash",
+
+];
+let otherSkillPercent=[
+    70,
+    60,
+    65,
+    60,
+    40
+];
+
+function addFrontSkill(){
+
+    for (i = 0; i < frontSkill.length; i++){
+
+        // add tag element
+        const title = document.createElement("p");
+        const progressDiv = document.createElement("div");
+        const progressBarDiv = document.createElement("div");
+
+        // add class element
+        title.classList.add("test");
+        progressDiv.classList.add("progress");
+        progressBarDiv.classList.add("progress-bar","progress-bar-striped","progress-bar-animated");
+
+        // add Id Element
+        title.setAttribute("id", "title"+frontSkill[i]);
+        progressDiv.setAttribute("id", "progressDiv"+frontSkill[i]);
+        progressBarDiv.setAttribute("id", "progressBarDiv"+frontSkill[i]);
+
+        // add content in tag
+        title.textContent = frontSkill[i];
+
+        progressBarDiv.setAttribute("role", "progressbar");
+        progressBarDiv.setAttribute("style", "width : "+ frontSkillPercent[i]+"%");
+        progressBarDiv.setAttribute("aria-valuenow", frontSkillPercent[i]);
+        progressBarDiv.setAttribute("aria-valuemin", "0");
+        progressBarDiv.setAttribute("aria-valuemax", "100");
+
+        //add element in HTML
+
+        document.getElementById("cardBodyFront").appendChild(title);
+        document.getElementById(title.id).appendChild(progressDiv);
+        document.getElementById(progressDiv.id).appendChild(progressBarDiv);
+
+        console.log("Test");
+    }
+}
+
+
+function addOtherSkill(){
+
+    for (i = 0; i < otherSkill.length; i++){
+
+        // add tag element
+        const title = document.createElement("p");
+        const progressDiv = document.createElement("div");
+        const progressBarDiv = document.createElement("div");
+
+        // add class element
+        title.classList.add("test");
+        progressDiv.classList.add("progress");
+        progressBarDiv.classList.add("progress-bar","progress-bar-striped","progress-bar-animated");
+
+        // add Id Element
+        title.setAttribute("id", "title"+otherSkill[i]);
+        progressDiv.setAttribute("id", "progressDiv"+otherSkill[i]);
+        progressBarDiv.setAttribute("id", "progressBarDiv"+otherSkill[i]);
+
+        // add content in tag
+        title.textContent = otherSkill[i];
+
+        progressBarDiv.setAttribute("role", "progressbar");
+        progressBarDiv.setAttribute("style", "width : "+ otherSkillPercent[i]+"%");
+        progressBarDiv.setAttribute("aria-valuenow", otherSkillPercent[i]);
+        progressBarDiv.setAttribute("aria-valuemin", "0");
+        progressBarDiv.setAttribute("aria-valuemax", "100");
+
+        //add element in HTML
+
+        document.getElementById("cardBodyOther").appendChild(title);
+        document.getElementById(title.id).appendChild(progressDiv);
+        document.getElementById(progressDiv.id).appendChild(progressBarDiv);
+
+        console.log("Test");
+    }
+}
+
+function addBackSkill(){
+
+    for (i = 0; i < backSkill.length; i++){
+
+        // add tag element
+        const title = document.createElement("p");
+        const progressDiv = document.createElement("div");
+        const progressBarDiv = document.createElement("div");
+
+        // add class element
+        title.classList.add("test");
+        progressDiv.classList.add("progress");
+        progressBarDiv.classList.add("progress-bar","progress-bar-striped","progress-bar-animated");
+
+        // add Id Element
+        title.setAttribute("id", "title"+backSkill[i]);
+        progressDiv.setAttribute("id", "progressDiv"+backSkill[i]);
+        progressBarDiv.setAttribute("id", "progressBarDiv"+backSkill[i]);
+
+        // add content in tag
+        title.textContent = backSkill[i];
+
+        progressBarDiv.setAttribute("role", "progressbar");
+        progressBarDiv.setAttribute("style", "width : "+ backSkillPercent[i]+"%");
+        progressBarDiv.setAttribute("aria-valuenow", backSkillPercent[i]);
+        progressBarDiv.setAttribute("aria-valuemin", "0");
+        progressBarDiv.setAttribute("aria-valuemax", "100");
+
+        //add element in HTML
+
+        document.getElementById("cardBodyBack").appendChild(title);
+        document.getElementById(title.id).appendChild(progressDiv);
+        document.getElementById(progressDiv.id).appendChild(progressBarDiv);
+
+        console.log("Test");
+    }
+}
+
